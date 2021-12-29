6c210eb543b1ccb2e56cbf6442ac51d9490107c0
dylan <dylan_alexandre@outlook.fr>
2021-12-29 01:24:37 +0100

    Base pour le projet

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,11 @@
+import modele.Julia;
+import org.apache.commons.math3.complex.Complex;
+
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args){
-        Julia j=new Julia.JuliaBuilder(1000,1,-0.7269,0.1889,1000,1000).build();
-        try {
-            j.drawJulia();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Julia j = new Julia.JuliaBuilder(1000, 2,new Complex (-0.7269, 0.1889), 0.01,1000,1,-1,1,-1).build();
+        j.drawFractal();
     }
 }
